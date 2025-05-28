d37ab32c7b14ec194c888e4c8c8b01805f6b640c
combine143 <combine142@gmail.com>
2025-05-28 14:37:07 +0300

    make build

---
+++ index.35aaccca.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".list-item__link"),r=document.querySelector(".gallery__large-img");e.forEach(function(e){e.addEventListener("click",function(t){t.preventDefault(),r.src=e.href})});
+//# sourceMappingURL=index.35aaccca.js.map
