ce28701a868218a8fdf0866373a702303a615a4b
combine143 <combine142@gmail.com>
2025-05-28 14:27:14 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.list-item__link');
+const target = document.querySelector('.gallery__large-img');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    target.src = button.href;
+  });
+});
